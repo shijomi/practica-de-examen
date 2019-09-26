ed2b97f026397f7948323d5e2ec9a32e3037ccdb
shijominagashiroq@gmail.com <shijominagashiroq@gmail.com>
2019-09-26 18:42:36 -0400

    segundo commit

---
+++ webEval/webEval/Models/Score.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace webEval.Models
+{    public enum Sex
+    {
+        masculino,
+        femenino,
+
+    }
+
+    public class Score
+    {
+        public int scoreID { get; set; }
+        public string name { get; set; }
+        public Sex sex { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime birthdate { get; set; }
+    }
+}
