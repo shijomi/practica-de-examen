4b932abf31fef7a667e8373050b9626964b78a3b
Shijomi Nagashiro <shijominagashiroq@gmail.com>
2019-09-27 14:38:08 -0400

    4to commit

---
+++ webEval/webEval/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace webEval.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext: DbContext
+    {
+        public DataContext():base ("DefaultConnection  ")
+        {
+
+        }
+    }
+}
